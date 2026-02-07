5ed8fa55e03930ebf37ea44330fc3fa03db32288
Suriya-002 <suriyarajavel02@gmail.com>
2026-02-06 21:16:13 -0500

    Use direct header assignment for CORS

--- backend/app.py
+++ backend/app.py
@@ -63,9 +63,9 @@ def create_app(config_class=Config):
 
     @app.after_request
     def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', 'https://quantum-tracker.onrender.com')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+        response.headers['Access-Control-Allow-Origin'] = 'https://quantum-tracker.onrender.com'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
         return response
 
     # Start background scheduler
@@ -79,6 +79,7 @@ app = create_app()
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
 
 
 
